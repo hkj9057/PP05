b777c12806a32a8e64f1460a7af92c59016b7b33
hkj9057@office.yuhan.ac.kr <hkj9057@office.yuhan.ac.kr>
2022-05-15 17:16:09 +0900

    [PP10_과제]

--- PP07/main.cpp
+++ PP07/main.cpp
@@ -1,6 +1,7 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <Windows.h>
 
 #pragma comment(lib,"OpenGL32")
 
@@ -16,6 +17,11 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 int main(void)
 {
     GLFWwindow* window;
+    float yPoint = 0.07f;
+    float yPoint2 = 0.0;;
+    float xPoint = 0.8f;
+    float xPoint2 = 0.6f;
+
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
@@ -38,29 +44,63 @@ int main(void)
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // 화면을 한가지 색으로 채운다(클리어하겠다)
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glPointSize(10);
+        if (GetAsyncKeyState(VK_SPACE) & 0x8000 || GetAsyncKeyState(VK_SPACE) & 0x8001)
+        {
+            yPoint = 0.27f;
+            yPoint2 = 0.20f;
+        }
+        else
+        {
+            yPoint = 0.07f;
+            yPoint2 = 0.0f;
+        }
+
+        glPointSize(5);
         glBegin(GL_TRIANGLES);
 
         glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex2f(0.5f, 0.5f);
+        glVertex2f(-0.07f, yPoint);
         glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex2f(0.5f, 0.0f);
+        glVertex2f(-0.07f, yPoint2);
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(-0.1f, 0.0f);
+        glVertex2f(0.0f, yPoint2);
 
         glEnd();
 
-        glPointSize(10);
+        glPointSize(5);
         glBegin(GL_TRIANGLES);
 
         glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(-0.07f, yPoint);
         glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex2f(0.5f, 0.5f);
+        glVertex2f(0.0f, yPoint);
         glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex2f(0.0f, -0.1f);
+        glVertex2f(0.0f, yPoint2);
 
         glEnd();
+
+        glPointSize(5);
+        glBegin(GL_TRIANGLES);
+
+        glColor3f(0.0f, 1.0f, 1.0f);
+        glVertex2f(xPoint2, 0.0f);
+        glColor3f(0.0f, 1.0f, 1.0f);
+        glVertex2f(xPoint, 0.0f);
+        glColor3f(0.0f, 0.0f, 0.0f);
+        glVertex2f(xPoint, 0.15f);
+
+        glEnd();
+
+        if (xPoint < -1.0f)
+            xPoint = 0.8f;
+        else
+            xPoint = xPoint - 0.003f;
+        if (xPoint2 < -1.2f)
+            xPoint2 = 0.6f;
+        else
+            xPoint2 = xPoint2 - 0.003f;
+        
+       
 
         glfwSwapBuffers(window);
         glfwPollEvents(); // 이벤트를 계속 체크
